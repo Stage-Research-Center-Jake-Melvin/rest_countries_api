2a1517b119d94f2fb57cb822c93e0d3936c336a3
jakemelvin <tmelvinjake@gmail.com>
2024-06-11 11:57:36 +0100

    feat: Modified the units in css and added theme management
    Fixed the simultaneous use of px and rem. Added to theme feature to switch from dark to light mode

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,18 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Navbar from "./navabar";
-
+import { useState } from "react";
+export type ColorMode = "Dark Mode" | "Light Mode";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [modeActuel, setModeActuel] = useState<ColorMode>("Dark Mode");
   return (
     <main className="app__main">
-      <Navbar />
+      <Navbar
+        modeActuel={modeActuel}
+        toggleFunction={setModeActuel}
+      />
     </main>
   );
 }
--- src/pages/navabar.tsx
+++ src/pages/navabar.tsx
@@ -1,10 +1,46 @@
-function Navbar() {
+import { Dispatch, SetStateAction, useState } from "react";
+import { ColorMode } from ".";
+
+interface ChangeMode {
+  modeActuel: ColorMode;
+  toggleFunction: Dispatch<SetStateAction<ColorMode>>;
+}
+
+function changeMode(): void {}
+function Navbar({ toggleFunction, modeActuel }: ChangeMode) {
+  function changeMode(): void {
+    if (modeActuel == "Dark Mode") {
+      toggleFunction("Light Mode");
+    } else {
+      toggleFunction("Dark Mode");
+    }
+  }
   return (
-    <div className="app__navbar">
-      <p className="app__navbar-title">Where in the world?</p>
-      <div className="app__navbar-mode">
-        <i className="fa-solid fa-moon"></i>
-        <p>Dark Mode</p>
+    <div
+      className={`app__navbar ${
+        modeActuel == "Dark Mode" ? "container_dark" : "container_light"
+      }`}
+    >
+      <p
+        className={`app__navbar-title ${
+          modeActuel == "Dark Mode" ? "dark" : "light"
+        }`}
+      >
+        Where in the world?
+      </p>
+      <div
+        className={`app__navbar-mode ${
+          modeActuel == "Dark Mode" ? "dark" : "light"
+        }`}
+        onClick={changeMode}
+      >
+        {modeActuel == "Dark Mode" ? (
+          <i className="fa-solid fa-moon"></i>
+        ) : (
+          <i className="fa-solid fa-sun"></i>
+        )}
+
+        <p>{modeActuel}</p>
       </div>
     </div>
   );
