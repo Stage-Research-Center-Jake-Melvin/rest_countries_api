b145f126c38653d9e63e716c1fb852720e5aed3a
jakemelvin <tmelvinjake@gmail.com>
2024-06-13 19:23:37 +0100

    feat: Adding the search functionality

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { MainSection, NavBar } from "@/components";
 import { ThemeContext } from "@/context/context";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import useDebounce from "@/hooks/useDebounce";
 
 /*Types*/
 export type Theme = "Dark Mode" | "Light Mode";
@@ -18,22 +19,28 @@ export default function Home() {
   const [showFilter, setShowFilter] = useState<boolean>(false);
   const [selectedRegion, setSelectedRegion] = useState<Region>();
   const [queryClient] = useState(() => new QueryClient());
-
+  const [search, setSearch] = useState<string>("");
+  const debouncedSearchTerm = useDebounce(search, 200);
   return (
-    <div>
-      <QueryClientProvider client={queryClient}>
-        <ThemeContext.Provider value={currentTheme}>
-          <main className="app__main">
-            <NavBar toggleFunction={setCurrentTheme} />
-            <MainSection
-              showFilter={showFilter}
-              setShowFilter={setShowFilter}
-              selectedContinent={selectedRegion}
-              changeContinent={setSelectedRegion}
-            />
-          </main>
-        </ThemeContext.Provider>
-      </QueryClientProvider>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <ThemeContext.Provider value={currentTheme}>
+        <main
+          className={`app__main ${
+            currentTheme == "Dark Mode" ? "container_dark" : "container_light"
+          }`}
+        >
+          <NavBar toggleFunction={setCurrentTheme} />
+          <MainSection
+            showFilter={showFilter}
+            setShowFilter={setShowFilter}
+            selectedContinent={selectedRegion}
+            changeContinent={setSelectedRegion}
+            searchValue={search}
+            changeSearchValue={setSearch}
+            debouncedSearchTerm={debouncedSearchTerm}
+          />
+        </main>
+      </ThemeContext.Provider>
+    </QueryClientProvider>
   );
 }
